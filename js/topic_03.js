5b47cbeba2efcd2c44f2897aeae180388e742895
Alex Kearns <alex.j.kearns@gmail.com>
2019-02-20 00:32:18 -0500

    Update topic_03.js

--- js/topic_03.js
+++ js/topic_03.js
@@ -29,31 +29,51 @@ function updateTable() {
     var creatureData = Data.creatures[select.value];
 
     table.innerHTML = 
-    '<tr><th>' + creatureData.type + '</th></tr>' +
-    '<tr>' + 
-        '<th>Emotions</th><th>Sounds</th><th>Legs</th><th>Rating</th>' +
+    '<tr>' +
+        '<th>' + creatureData.type + '</th>' +
     '</tr>' +
-    '<tr><td><ul>';
+    '<tr>' + 
+        '<th>Emotions</th>' +
+        '<th>Sounds</th>' +
+        '<th>Legs</th>' +
+        '<th>Rating</th>' +
+    '</tr>' +
+    '<tr>' +
+        '<td>' +
+            '<ul>';
 
     // add list of emotions
     creatureData.emotions.forEach(function(element) {
         table.innerHTML += '<li>'+ element + '</li>';
     });
 
-    table.innerHTML += '</ul></td><td><ul>';
+    table.innerHTML += 
+            '</ul>' +
+        '</td>' + 
+        '<td>' +
+            '<ul>';
 
     // add list of sounds
     creatureData.sounds.forEach(function(element) {
         table.innerHTML += '<li>'+ element + '</li>';
     });
 
-    table.innerHTML += '</ul></td><td>';
+    table.innerHTML += 
+            '</ul>' +
+        '</td>' + 
+        '<td>';
 
     // add the legs
-    table.innerHTML += creatureData.legs + '</td><td>';
+    table.innerHTML += 
+            creatureData.legs + 
+        '</td>' +
+        '<td>';
 
     // add the rating
-    table.innerHTML += creatureData.rating + '</td>';
+    table.innerHTML += 
+            creatureData.rating + 
+        '</td>' +
+    '</tr>';
 
     table.classList.remove("hide");
 }
