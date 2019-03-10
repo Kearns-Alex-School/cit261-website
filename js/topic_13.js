a212751e339176e9f77ae9654bddb5346cd038dc
Alex Kearns <alex.j.kearns@gmail.com>
2019-03-10 15:35:58 -0400

    Final submission
    All topics should be covered by now and ready

--- js/topic_13.js
+++ js/topic_13.js
@@ -1,5 +1,57 @@
 window.intervalid = '';
 
+function audioPlayer(){
+    $("#audio")[0].src = $("#playlist li a")[0];
+    $("#playlist li a").click(function(e){
+       e.preventDefault(); 
+       $("#audio")[0].src = this;
+       $("#playlist li").removeClass("active");
+        $(this).parent().addClass("active");
+    });
+}
+
+function setupPaint() {
+    var canvas = document.getElementById('myCanvas');
+    var ctx = canvas.getContext('2d');
+    
+    var painting = document.getElementById('paint');
+    var paint_style = getComputedStyle(painting);
+    canvas.width = parseInt(paint_style.getPropertyValue('width'));
+    canvas.height = parseInt(paint_style.getPropertyValue('height'));
+
+    var mouse = {x: 0, y: 0};
+    
+    canvas.addEventListener('mousemove', function(e) {
+    mouse.x = e.pageX - this.offsetLeft;
+    mouse.y = e.pageY - this.offsetTop;
+    ctx.lineWidth = document.getElementById("brush").value;
+    ctx.strokeStyle = document.getElementById("color").value;
+    }, false);
+
+    ctx.lineJoin = 'round';
+    ctx.lineCap = 'round';
+    
+    canvas.addEventListener('mousedown', function(e) {
+        ctx.beginPath();
+        ctx.moveTo(mouse.x, mouse.y);
+    
+        canvas.addEventListener('mousemove', onPaint, false);
+    }, false);
+    
+    canvas.addEventListener('mouseup', function() {
+        canvas.removeEventListener('mousemove', onPaint, false);
+    }, false);
+
+    canvas.addEventListener('mouseleave', function() {
+        canvas.removeEventListener('mousemove', onPaint, false);
+    })
+    
+    var onPaint = function() {
+        ctx.lineTo(mouse.x, mouse.y);
+        ctx.stroke();
+    };
+}
+
 function starting() {
     var metronome = document.getElementById("metronome");
     var tempo = document.getElementById("tempo").value;
@@ -59,16 +111,4 @@ function resetting() {
 
     breathe_text.innerText = "Breathe In";
     clearInterval(intervalid);
-}
-
-function audioPlayer(){
-    var currentSong = 0;
-    $("#audio")[0].src = $("#playlist li a")[0];
-    $("#playlist li a").click(function(e){
-       e.preventDefault(); 
-       $("#audio")[0].src = this;
-       $("#playlist li").removeClass("active");
-        currentSong = $(this).parent().index();
-        $(this).parent().addClass("active");
-    });
 }
