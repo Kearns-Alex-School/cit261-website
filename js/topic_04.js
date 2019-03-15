774310595b88eec812a86797c6dfcb06eace2ab2
Kearns-Alex <kea12002@byui.edu>
2019-03-15 14:15:34 -0400

    updated fix
    Call works and now we just need to grab the info and fill it into our table

--- js/topic_04.js
+++ js/topic_04.js
@@ -71,26 +71,102 @@ function load() {
 
 function loadWeather() {
   // Create a request variable and assign a new XMLHttpRequest object to it.
-  var request = new XMLHttpRequest()
+  var request = new XMLHttpRequest();
 
   // Open a new connection, using the GET request on the URL endpoint
-  request.open('GET', 'https://api.openweathermap.org/data/2.5/weather?zip=94040&appid=16872869c55bacf84d3ac635e9c25bc6', true)
+  request.open('GET', 'https://api.openweathermap.org/data/2.5/weather?zip=48111,us&units=imperial&appid=16872869c55bacf84d3ac635e9c25bc6', true);
 
   request.onload = function () {
     // Begin accessing JSON data here
-    console.log(this.response)
-    var data = JSON.parse(this.response)
+    console.log(this.response);
+    var data = JSON.parse(this.response);
 
     if (request.status >= 200 && request.status < 400) {
-      /*data.forEach(movie => {
-        console.log(movie.title)
-      })*/
-      console.log(data)
+      console.log(data);
+
+      updateResults(data);
     } else {
-      console.log('error')
+      console.log('error');
     }
   }
 
   // Send request
-  request.send()
+  request.send();
 }
+
+function updateResults(data) {
+  var table = document.getElementById("weather_results");
+
+  var newHTML = 
+  '<tr>' +
+      '<th>Location</th>' +
+      '<td>' + '</td>' +
+  '</tr>' +
+  '<tr>' +
+      '<th>Current</th>' +
+      '<td>' + '</td>' +
+  '</tr>' +
+  '<tr>' +
+      '<th>High/Low</th>' +
+      '<td>' + '</td>' +
+  '</tr>' +
+  '<tr>' +
+      '<th>Humidity</th>' +
+      '<td>' + '</td>' +
+  '</tr>' +
+  '<tr>' +
+      '<th>Wind Speed</th>' +
+      '<td>' + '</td>' +
+  '</tr>' +
+  '<th>Sunrise</th>' +
+    '<td>' + '</td>' +
+  '</tr>' +
+  '<th>Sunset</th>' +
+    '<td>' + '</td>' +
+  '</tr>';
+
+  table.innerHTML = newHTML;
+}
+
+/******
+* Parameters:
+coord
+coord.lon City geo location, longitude
+coord.lat City geo location, latitude
+weather (more info Weather condition codes)
+weather.id Weather condition id
+weather.main Group of weather parameters (Rain, Snow, Extreme etc.)
+weather.description Weather condition within the group
+weather.icon Weather icon id
+base Internal parameter
+main
+main.temp Temperature. Unit Default: Kelvin, Metric: Celsius, Imperial: Fahrenheit.
+main.pressure Atmospheric pressure (on the sea level, if there is no sea_level or grnd_level data), hPa
+main.humidity Humidity, %
+main.temp_min Minimum temperature at the moment. This is deviation from current temp that is possible for large cities and megalopolises geographically expanded (use these parameter optionally). Unit Default: Kelvin, Metric: Celsius, Imperial: Fahrenheit.
+main.temp_max Maximum temperature at the moment. This is deviation from current temp that is possible for large cities and megalopolises geographically expanded (use these parameter optionally). Unit Default: Kelvin, Metric: Celsius, Imperial: Fahrenheit.
+main.sea_level Atmospheric pressure on the sea level, hPa
+main.grnd_level Atmospheric pressure on the ground level, hPa
+wind
+wind.speed Wind speed. Unit Default: meter/sec, Metric: meter/sec, Imperial: miles/hour.
+wind.deg Wind direction, degrees (meteorological)
+clouds
+clouds.all Cloudiness, %
+rain
+rain.1h Rain volume for the last 1 hour, mm
+rain.3h Rain volume for the last 3 hours, mm
+snow
+snow.1h Snow volume for the last 1 hour, mm
+snow.3h Snow volume for the last 3 hours, mm
+dt Time of data calculation, unix, UTC
+sys
+sys.type Internal parameter
+sys.id Internal parameter
+sys.message Internal parameter
+sys.country Country code (GB, JP etc.)
+sys.sunrise Sunrise time, unix, UTC
+sys.sunset Sunset time, unix, UTC
+id City ID
+name City name
+cod Internal parameter
+*/
