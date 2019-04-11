52b2a8a670f2ee46ebf1a9f0b55729ae836612e1
Alex Kearns <alex.j.kearns@gmail.com>
2019-04-10 23:59:59 -0400

    Finished!
    Final is done
    http://ec2-3-18-11-37.us-east-2.compute.amazonaws.com/final/chat.html

--- final/chat.js
+++ final/chat.js
@@ -5,12 +5,82 @@ window.file;
 
 var chat          = new Chat();
 
+function Setup() {
+  var data
+  // handle all of the initial loading of our settings
+  if (localStorage.getItem("settings") !== null) {
+    data = JSON.parse(localStorage.getItem("settings"));
+
+    var table = document.getElementById("user-settings");
+
+    for (var rowindex = 0, row; row = table.rows[rowindex]; rowindex++) {
+      row.cells[1].children[0].value = data[rowindex];
+    }
+  }
+  else {
+    data = ['#919191', '#444444', '#444444', '#444444', '#ffffff', '#000000', '#000000', '#ffffff', 'Guest', 'Public'];
+
+    var table = document.getElementById("user-settings");
+
+    // populate the list of settings
+    for (var rowindex = 0, row; row = table.rows[rowindex]; rowindex++) {
+      row.cells[1].children[0].value = data[rowindex];
+    }
+  }
+
+  // make the changes
+  document.body.style.backgroundColor = data[0];
+
+  document.getElementById('login-block').style.backgroundColor = data[1];
+
+  document.getElementById('main').style.backgroundColor = data[2];
+
+  document.getElementById('option').style.backgroundColor = data[3];
+
+  var yours = document.getElementsByClassName("yours");
+  for(var index = 0; index < yours.length; index++) {
+    yours[index].style.backgroundColor = data[4];
+    yours[index].style.color = data[5];
+  }
+
+  var theirs = document.getElementsByClassName("theirs");
+  for(var index = 0; index < theirs.length; index++) {
+    theirs[index].style.backgroundColor = data[6];
+    theirs[index].style.color = data[7];
+  }
+
+  document.getElementById("user").value = localStorage.getItem("user");
+  document.getElementById("file").value = localStorage.getItem("file");
+}
+
+function ClearSettings() {
+  // clear the local storage
+  localStorage.clear();
+
+  Setup();
+}
+
+function SaveSettings() {
+  var data = [];
+  var table = document.getElementById("user-settings");
+
+  for (var rowindex = 0, row; row = table.rows[rowindex]; rowindex++) {
+    data.push(row.cells[1].children[0].value);
+  }
+
+  // send the array to local storage via JSON
+  localStorage.setItem('settings', JSON.stringify(data));
+
+  Setup();
+}
+
 function Login(action) {
   // set up all of the variables that we could be using here
   var loginbutton  = document.getElementById("login");
   var logoutbutton = document.getElementById("logout");
   var mainchat     = document.getElementById("main");
   var chatbox      = document.getElementById("chat");
+  var optionbox    = document.getElementById("option");
   var user         = document.getElementById("user");
   var file         = document.getElementById("file");
   var greating     = document.getElementById("greeting");
@@ -33,6 +103,7 @@ function Login(action) {
       
       // make our chat areas and logout visable
       mainchat.classList.remove("hide");
+      optionbox.classList.remove("hide");
       logoutbutton.classList.remove("hide");
 
       // make the login button hidden
@@ -43,7 +114,7 @@ function Login(action) {
       file.disabled = true;
     
       // greet our user
-      greating.innerHTML = "Welcome " + user.value;
+      greating.innerHTML = "Welcome " + user.value + "!";
     
       // update what is in the chat
       chat.update();
@@ -59,6 +130,7 @@ function Login(action) {
 
       // make our chat areas and logout hidden
       mainchat.classList.add("hide");
+      optionbox.classList.add("hide");
       logoutbutton.classList.add("hide");
 
       // make the login button visable
@@ -72,6 +144,9 @@ function Login(action) {
       file.disabled  = false;
       break;
   }
+
+  localStorage.setItem('user', user.value);
+  localStorage.setItem('file', file.value);
 }
 
 function CheckEnterPressed() {
@@ -130,7 +205,7 @@ function UpdateChat() {
 					for (var i = 0; i < data.text.length; i++) {
             // parse out the line and assign them to variables
             var array = data.text[i].split("|");
-            var muser = array[0];
+            var messenger = array[0];
             var message = array[1];
             var time = array[2];
 
@@ -139,19 +214,20 @@ function UpdateChat() {
 
             // build what we will put inside
             var html = 
-              "<p>" + muser + "</p>" + 
+              "<p>" + messenger + "</p>" + 
               "<p>" + message + "</p>";
 
-            // add the container class to the node
-            node.classList.add('container');
+            // add the messagebox class to the node
+            node.classList.add('messagebox');
 
             // decide if we are the user or not
-            if (muser !== user.value) {
-              node.classList.add('darker');
+            if (messenger == user.value) {
+              node.classList.add('yours');
               html = html +
                 '<span class="time-right">' + time + '</span>';
             }
             else {
+              node.classList.add('theirs');
               html = html +
                 '<span class="time-left">' + time + '</span>';
             }
@@ -161,6 +237,9 @@ function UpdateChat() {
 
             // add the node to the chat window
             chat.appendChild(node);
+
+            // update the new messages with the saved settings
+            Setup();
           }	
           
           // scroll to show the most recent message that was added
